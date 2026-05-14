5f9e8c92916af80ba68895484c9df8d5b540f7b3
omas <omas7th@gmail.com>
2026-05-14 20:38:36 +0100

    update

--- server.js
+++ server.js
@@ -33,8 +33,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Explicitly handle preflight requests
-app.options('*', cors());
 // -------------------------------------------
 
 app.use(express.json());
